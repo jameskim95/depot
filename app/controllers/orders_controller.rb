45b043e56f00e2dc88f58f209e7040e6c0303b14
James Kim <jkim@taphere.com>
2016-06-13 15:53:56 -0400

    Depot 16

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,6 +38,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:card_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver
 
         format.html { redirect_to @order, notice: 'Thank you for your order.' }
         format.json { render action: 'show', status: :created, location: @order }
